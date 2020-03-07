2947b4b20e427f74f09f2c5c938f3750720d872a
thakkar <thakkar@LAPTOP-OI7FTNRE>
2020-03-07 18:35:22 +0530

    final csharp automation project with lic site demo

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace CsharpProject
 {
     class Program
     {
-        static void Main(string[] args)
+        
+       public  static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IWebDriver driver;
+            Console.WriteLine("welcome to lic program");
+        
+        
+            //load chromedriver 
+            driver = new ChromeDriver();
+
+           // access url
+            driver.Navigate().GoToUrl("https://licindia.in/");
+        //}
+
+        //[Test]
+      //  public void ExecuteTest()
+        //{
+
+            //maximize window
+            driver.Manage().Window.Maximize();
+
+            //click on customer portal
+            SetterGetterMethods.Clickelement(driver, "//*[@id='mid']/div[1]/div[1]/h2/a[3]", "XPath");
+
+            // handle alert
+            driver.SwitchTo().Alert().Accept();
+            System.Threading.Thread.Sleep(5000);
+
+            //click on new user button
+            SetterGetterMethods.Clickelement(driver, "//*[@id='container']/article/p[2]/input[1]", "XPath");
+            System.Threading.Thread.Sleep(5000);
+
+            //enter value in policy textbox
+            SetterGetterMethods.SendkeysMethod(driver, "da_textfield-1143-inputEl", "Id", "123456789");
+
+            //enter value in installment premium without tax 
+            SetterGetterMethods.SendkeysMethod(driver, "DScurrencyfield-1151-inputEl", "Id", "100000");
         }
+
+       // [Test]
+        //public void Cleanup()
+        //{
+          //  driver.Close();
+        //}
     }
 }
---
+++ CsharpProject/SetterGetterMethods.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpProject
+{
+    class SetterGetterMethods
+    {
+        //Click element method
+        public static String Clickelement(IWebDriver driver,String element,String elementType)
+        {
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+            }
+            if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click();
+            }
+            if (elementType == "XPath")
+            {
+                driver.FindElement(By.XPath(element)).Click();
+            }
+            
+                return string.Empty;
+        }
+        //Sendkeys method
+        public  static String SendkeysMethod(IWebDriver driver, String element, String elementType,String value)
+        {
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            }
+            if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            }
+            if (elementType == "XPath")
+            {
+                driver.FindElement(By.XPath(element)).SendKeys(value);
+            }
+           
+                return string.Empty;
+        }
+    }
+}
